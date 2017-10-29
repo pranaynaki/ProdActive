0b18836a2346eb3bd939844a3f4ee0e06ecf5154
pranaynaki <pranaynaki@gmail.com>
2017-10-29 02:02:24 -0500

    activity.py the durations should be set

--- flaskproject/activity.py
+++ flaskproject/activity.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import json
 
 class Activity():
@@ -14,22 +15,43 @@ class Activity():
 class ActivityManager():
 
     def __init__(self):
-        self.rawLog = []
-        self.dedupLog = []
+        # self.rawLog = []
+        # self.dedupLog = []
+        self.last_domain = None
+        self.last_time = None
+        self.raw_durations = []
+        self.durations = {}
     
     def add(self, name, duration = None):
         self.rawLog.append(Activity(name))
-        for activity in self.dedupLog:
-            if activity.name == name:
-                if activity.duration != None:
-                    activity.duration += duration
-                return
-        self.dedupLog.append(Activity(name, duration))
+        # for activity in self.dedupLog:
+        #     if activity.name == name:
+        #         if activity.duration != None:
+        #             activity.duration += duration
+        #         return
+        # self.dedupLog.append(Activity(name, duration))
+        #self.raw_durations.append({"name": name, "duration": duration})
+        if self.last_domain is None:
+            self.last_domain = name
+            self.last_time = datetime.now()
+            self.durations[name] = 0
+        else:
+            if name not in self.durations:
+                self.durations[name] = 0
+            current_time = datetime.now()
+            self.durations[self.last_domain] += current_time - last_time
+            self.last_domain = name
+            self.last_time = current_time
+
+        
+        
 
     def getRawLog(self):
         return [a.to_dict() for a in self.rawLog]
 
     def getDedDupLog(self):
-        return [a.to_dict() for a in self.dedupLog]
-        
+        # return [a.to_dict() for a in self.dedupLog]
+        return [{"name": name, "duration": self.durations[name]} for name in self.durations.keys()]
+        # for ind, name, duration in enumerate(self.durations):
+
 
